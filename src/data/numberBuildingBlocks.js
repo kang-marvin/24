889d9548c80c912cdf20e5acf24c1273b3313564
Marvin Kang'ethe <marvin.kangethe@andela.com>
2019-10-23 15:28:16 -0400

    Create building blocks for numbers 0 to 9

---
+++ src/data/numberBuildingBlocks.js
@@ -0,0 +1,152 @@
+export const numberBuildingBlocks = {
+  "0": {
+    "leftComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    },
+    "centerComponent": {
+      "showTop": "true",
+      "showCenter": "false",
+      "showBottom": "true"
+    },
+    "rightComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    }
+  },
+  "1": {
+    "leftComponent": {
+      "showTop": "false",
+      "showBottom": "false"
+    },
+    "centerComponent": {
+      "showTop": "false",
+      "showCenter": "false",
+      "showBottom": "false"
+    },
+    "rightComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    }
+  },
+  "2": {
+    "leftComponent": {
+      "showTop": "false",
+      "showBottom": "true"
+    },
+    "centerComponent": {
+      "showTop": "true",
+      "showCenter": "true",
+      "showBottom": "true"
+    },
+    "rightComponent": {
+      "showTop": "true",
+      "showBottom": "false"
+    }
+  },
+  "3": {
+    "leftComponent": {
+      "showTop": "false",
+      "showBottom": "false"
+    },
+    "centerComponent": {
+      "showTop": "true",
+      "showCenter": "true",
+      "showBottom": "true"
+    },
+    "rightComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    }
+  },
+  "4": {
+    "leftComponent": {
+      "showTop": "true",
+      "showBottom": "false"
+    },
+    "centerComponent": {
+      "showTop": "false",
+      "showCenter": "true",
+      "showBottom": "false"
+    },
+    "rightComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    }
+  },
+  "5": {
+    "leftComponent": {
+      "showTop": "true",
+      "showBottom": "false"
+    },
+    "centerComponent": {
+      "showTop": "true",
+      "showCenter": "true",
+      "showBottom": "true"
+    },
+    "rightComponent": {
+      "showTop": "false",
+      "showBottom": "true"
+    }
+  },
+  "6": {
+    "leftComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    },
+    "centerComponent": {
+      "showTop": "true",
+      "showCenter": "true",
+      "showBottom": "true"
+    },
+    "rightComponent": {
+      "showTop": "false",
+      "showBottom": "true"
+    }
+  },
+  "7": {
+    "leftComponent": {
+      "showTop": "false",
+      "showBottom": "false"
+    },
+    "centerComponent": {
+      "showTop": "true",
+      "showCenter": "false",
+      "showBottom": "false"
+    },
+    "rightComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    }
+  },
+  "8": {
+    "leftComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    },
+    "centerComponent": {
+      "showTop": "true",
+      "showCenter": "true",
+      "showBottom": "true"
+    },
+    "rightComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    }
+  },
+  "9": {
+    "leftComponent": {
+      "showTop": "true",
+      "showBottom": "false"
+    },
+    "centerComponent": {
+      "showTop": "true",
+      "showCenter": "true",
+      "showBottom": "true"
+    },
+    "rightComponent": {
+      "showTop": "true",
+      "showBottom": "true"
+    }
+  }
+};
