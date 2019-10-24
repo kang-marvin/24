2b26cc2dff0d8a05374582fe3b1a825f661b0d0a
Marvin Kang'ethe <marvin.kangethe@andela.com>
2019-10-24 18:42:40 +0300

    Pass timer to NumberComponent and setInterval to update the timer value

--- src/containers/guests/TwentyFourCounter.js
+++ src/containers/guests/TwentyFourCounter.js
@@ -2,14 +2,33 @@ import React from "react";
 
 import NumberComponent from "../../components/display/NumberComponent";
 
+import timerUtils from "../../utils/timerUtils";
+
 import "../../styles/index.css";
+
 class TwentyFourCounter extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      counter: '24:00:00'
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      const currentTimerCount = new timerUtils().decrementCounter(this.state.counter)
+      const nextTimerCount = new timerUtils().decrementCounter(currentTimerCount.nextCounter)
+      this.setState({counter: nextTimerCount.currentCounter})
+    }, 1000);
+  }
 
   render() {
     return (
       <div className="-card">
         <div className="card-body">
-          <NumberComponent counter='88:88:88' />
+          <NumberComponent
+            counter={ this.state.counter } />
         </div>
       </div>
     );
